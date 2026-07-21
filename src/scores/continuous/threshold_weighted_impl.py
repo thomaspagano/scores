556e996a9122ddd9d21f96c7d779e90243632271
Tom Pagano (he/him) <tompagan@bom.gov.au>
2026-07-21 23:06:02 +0000

    Split uplift work into issue-1057-references-10-continuous-threshold

--- src/scores/continuous/threshold_weighted_impl.py
+++ src/scores/continuous/threshold_weighted_impl.py
@@ -387,29 +387,10 @@ def tw_squared_error(
         ValueError: if ``interval_where_one`` and ``interval_where_positive`` do not
             specify a valid trapezoidal weight.
 
-    Reference:
-        Taggart, R. (2022). Evaluation of point forecasts for extreme events using
-        consistent scoring functions. Quarterly Journal of the Royal Meteorological
-        Society, 148(742), 306-320. https://doi.org/10.1002/qj.4206
-
-    Examples:
-        >>> import numpy as np
-        >>> import xarray as xr
-        >>> from scores.continuous import tw_squared_error
-
-        >>> # Create forecast and observation data
-        >>> fcst = xr.DataArray([1.0, 2.0, 3.0], dims=["time"])
-        >>> obs = xr.DataArray([1.5, 2.5, 2.0], dims=["time"])
-
-        >>> tw_squared_error(fcst, obs, interval_where_one=(0, 3))
-        <xarray.DataArray ()> Size: 8B
-        array(0.5)
-
-        >>> tw_squared_error(
-        ...     fcst, obs, interval_where_one=(1, 2), interval_where_positive=(0, 3)
-        ... )
-        <xarray.DataArray ()> Size: 8B
-        array(0.26388889)
+    References:
+        - Taggart, R. (2022). Evaluation of point forecasts for extreme events using
+          consistent scoring functions. Quarterly Journal of the Royal Meteorological
+          Society, 148(742), 306–320. https://doi.org/10.1002/qj.4206
     """
     _check_tws_args(interval_where_one=interval_where_one, interval_where_positive=interval_where_positive)
     _, phi, phi_prime = _auxiliary_funcs(fcst, obs, interval_where_one, interval_where_positive)
@@ -491,26 +472,9 @@ def tw_absolute_error(
             specify a valid trapezoidal weight.
 
     References:
-        Taggart, R. (2022). Evaluation of point forecasts for extreme events using
-        consistent scoring functions. Quarterly Journal of the Royal Meteorological
-        Society, 148(742), 306-320. https://doi.org/10.1002/qj.4206
-
-    Examples:
-        >>> import xarray as xr
-        >>> from scores.continuous import tw_absolute_error
-
-        >>> fcst = xr.DataArray([1.0, 2.0, 3.0], dims=["time"])
-        >>> obs = xr.DataArray([1.5, 2.5, 2.0], dims=["time"])
-
-        >>> tw_absolute_error(fcst, obs, interval_where_one=(0, 3))
-        <xarray.DataArray ()> Size: 8B
-        array(0.66666667)
-
-        >>> tw_absolute_error(
-        ...     fcst, obs, interval_where_one=(1, 2), interval_where_positive=(0, 3)
-        ... )
-        <xarray.DataArray ()> Size: 8B
-        array(0.45833333)
+        - Taggart, R. (2022). Evaluation of point forecasts for extreme events using
+          consistent scoring functions. Quarterly Journal of the Royal Meteorological
+          Society, 148(742), 306–320. https://doi.org/10.1002/qj.4206
     """
     _check_tws_args(interval_where_one=interval_where_one, interval_where_positive=interval_where_positive)
     g, _, _ = _auxiliary_funcs(fcst, obs, interval_where_one, interval_where_positive)
@@ -596,31 +560,9 @@ def tw_quantile_score(
             specify a valid trapezoidal weight.
 
     References:
-        Taggart, R. (2022). Evaluation of point forecasts for extreme events using
-        consistent scoring functions. Quarterly Journal of the Royal Meteorological
-        Society, 148(742), 306-320. https://doi.org/10.1002/qj.4206
-
-    Examples:
-        >>> import xarray as xr
-        >>> from scores.continuous import tw_quantile_score
-
-        >>> fcst = xr.DataArray([1.0, 2.0, 3.0], dims=["time"])
-        >>> obs = xr.DataArray([1.5, 2.5, 2.0], dims=["time"])
-
-        >>> tw_quantile_score(fcst, obs, interval_where_one=(0, 3), alpha=0.5)
-        <xarray.DataArray ()> Size: 8B
-        array(0.33333333)
-
-        >>> tw_quantile_score(
-        ...     fcst,
-        ...     obs,
-        ...     interval_where_one=(1, 2),
-        ...     interval_where_positive=(0, 3),
-        ...     alpha=0.5,
-        ... )
-        <xarray.DataArray ()> Size: 8B
-        array(0.22916667)
-
+        - Taggart, R. (2022). Evaluation of point forecasts for extreme events using
+          consistent scoring functions. Quarterly Journal of the Royal Meteorological
+          Society, 148(742), 306–320. https://doi.org/10.1002/qj.4206
     """
 
     check_alpha(alpha)
@@ -706,31 +648,9 @@ def tw_expectile_score(
             specify a valid trapezoidal weight.
 
     References:
-        Taggart, R. (2022). Evaluation of point forecasts for extreme events using
-        consistent scoring functions. Quarterly Journal of the Royal Meteorological
-        Society, 148(742), 306-320. https://doi.org/10.1002/qj.4206
-
-    Examples:
-        >>> import xarray as xr
-        >>> from scores.continuous import tw_expectile_score
-
-        >>> fcst = xr.DataArray([1.0, 2.0, 3.0], dims=["time"])
-        >>> obs = xr.DataArray([1.5, 2.5, 2.0], dims=["time"])
-
-        >>> tw_expectile_score(fcst, obs, interval_where_one=(0, 3), alpha=0.5)
-        <xarray.DataArray ()> Size: 8B
-        array(0.25)
-
-        >>> tw_expectile_score(
-        ...     fcst,
-        ...     obs,
-        ...     interval_where_one=(1, 2),
-        ...     interval_where_positive=(0, 3),
-        ...     alpha=0.5,
-        ... )
-        <xarray.DataArray ()> Size: 8B
-        array(0.13194444)
-
+        - Taggart, R. (2022). Evaluation of point forecasts for extreme events using
+          consistent scoring functions. Quarterly Journal of the Royal Meteorological
+          Society, 148(742), 306–320. https://doi.org/10.1002/qj.4206
     """
 
     check_alpha(alpha)
@@ -819,31 +739,9 @@ def tw_huber_loss(
             specify a valid trapezoidal threshold weight.
 
     References:
-        Taggart, R. (2022). Evaluation of point forecasts for extreme events using
-        consistent scoring functions. Quarterly Journal of the Royal Meteorological
-        Society, 148(742), 306-320. https://doi.org/10.1002/qj.4206
-
-    Examples:
-        >>> import xarray as xr
-        >>> from scores.continuous import tw_huber_loss
-
-        >>> fcst = xr.DataArray([1.0, 2.0, 3.0], dims=["time"])
-        >>> obs = xr.DataArray([1.5, 2.5, 2.0], dims=["time"])
-
-        >>> tw_huber_loss(fcst, obs, huber_param=0.5, interval_where_one=(0, 3))
-        <xarray.DataArray ()> Size: 8B
-        array(0.20833333)
-
-        >>> tw_huber_loss(
-        ...     fcst,
-        ...     obs,
-        ...     huber_param=1.0,
-        ...     interval_where_one=(1, 2),
-        ...     interval_where_positive=(0, 3),
-        ... )
-        <xarray.DataArray ()> Size: 8B
-        array(0.13194444)
-
+        - Taggart, R. (2022). Evaluation of point forecasts for extreme events using
+          consistent scoring functions. Quarterly Journal of the Royal Meteorological
+          Society, 148(742), 306–320. https://doi.org/10.1002/qj.4206
     """
 
     check_huber_param(huber_param)
